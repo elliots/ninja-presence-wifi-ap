4e52c7848bb8bf7eab093b90e1b1d99d579927f4
Elliot Shepherd <elliot@jarofworms.com>
2013-05-30 22:10:58 +1000

    Initial version using presence base class

---
+++ README.md
@@ -0,0 +1,7 @@
+ninja-presence-wifi-ap
+======
+
+This is a presence driver for the NinjaBlocks client that tells you when you enter of leave the range of wifi towers.
+By default there is a 1min timeout but this will be configurable through the NinjaBlocks dashboard later.
+
+Only works on *nix and osx.
---
+++ index.js
@@ -0,0 +1,26 @@
+var Presence = require('ninja-presence-base');
+var wifiLocation = require('wifi_location');
+
+module.exports = Presence;
+
+Presence.prototype.G = 'wifi';
+Presence.prototype.scan = function() {
+  var self = this;
+
+  wifiLocation.wifiTowers(function(err, towers) {
+    if (err) {
+      self._app.log.warn('Failed to get Wifi access points', err);
+    } else {
+      towers.forEach(function(tower) {
+          self.see({
+                name: tower.ssid,
+                id: tower.mac_address,
+                distance: Math.abs(tower.signal_strength)
+          });
+      });
+    }
+
+    self.scanComplete();
+  });
+
+};
